a905d79d90cab952cba692e3b8ff0ffdefa67378
bigway <bigway2626@gmail.com>
2017-10-03 23:02:07 +0800

    合併個人資訊

---
+++ src/assets/js/profile2.js
@@ -0,0 +1,48 @@
+// html結合
+$(document).on('ready', function () {
+    $("#contacts-section").load("footer.html");
+   
+    $(".copyright").load("copyright.html");
+    $(".pro_contents2").load("pro_contents2.html");
+});
+
+$(document).on('ready', function () {
+    // initialization of go to
+    $.HSCore.components.HSGoTo.init('.js-go-to');
+
+    // initialization of tabs
+    $.HSCore.components.HSTabs.init('[role="tablist"]');
+
+    // initialization of chart pies
+    var items = $.HSCore.components.HSChartPie.init('.js-pie');
+
+    // initialization of HSScrollBar component
+    $.HSCore.components.HSScrollBar.init($('.js-scrollbar'));
+});
+
+$(window).on('load', function () {
+    // initialization of header
+    $.HSCore.components.HSHeader.init($('#js-header'));
+    $.HSCore.helpers.HSHamburgers.init('.hamburger');
+
+    // initialization of HSMegaMenu component
+    $('.js-mega-menu').HSMegaMenu({
+        event: 'hover',
+        pageContainer: $('.container'),
+        breakpoint: 991
+    });
+
+    // initialization of horizontal progress bars
+    setTimeout(function () { // important in this case
+        var horizontalProgressBars = $.HSCore.components.HSProgressBar.init('.js-hr-progress-bar', {
+            direction: 'horizontal',
+            indicatorSelector: '.js-hr-progress-bar-indicator'
+        });
+    }, 1);
+});
+
+$(window).on('resize', function () {
+    setTimeout(function () {
+        $.HSCore.components.HSTabs.init('[role="tablist"]');
+    }, 200);
+});
